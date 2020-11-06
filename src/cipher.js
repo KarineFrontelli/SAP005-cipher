d07857e4421854e758941348e65956e6783ecf50
karinefrontelli <karinefrontellidossantos@gmail.com>
2020-11-06 17:20:40 -0300

    Projeto Cipher Finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,34 +1,39 @@
 const tamanhoDoAlfabeto = 26;
 let codigo1Letra = 65;
 
-// Função criptar
 const cipher = {
   encode: function encode(offset, string) {
-    if (offset == null || !string){
+    if (offset == null || !string) {
       throw new TypeError("Erro");
-    }else{
-    let resultadoCifrado = ""
-    for (let i = 0; i < string.length; i++) {
-      const cifrar = string.charCodeAt(i);
-      const codificado = ((cifrar - codigo1Letra + offset) % tamanhoDoAlfabeto) + codigo1Letra;
-      resultadoCifrado += String.fromCharCode(codificado);
+    } else {
+      let resultadoCifrado = ""
+      for (let i = 0; i < string.length; i++) {
+        const cifrar = string.charCodeAt(i);
+        let codificado = ((cifrar - codigo1Letra + offset) % tamanhoDoAlfabeto) + codigo1Letra;
+        if (cifrar <= 64 || cifrar >= 91) {
+          codificado = cifrar;
+        }
+        resultadoCifrado += String.fromCharCode(codificado);
+      }
+      return resultadoCifrado
     }
-    return resultadoCifrado
-  }
   },
-  //Função descriptar
+
   decode: function decode(offset, string) {
-    if (offset == null || !string){
+    if (offset == null || !string) {
       throw new TypeError("Erro");
-    }else{
+    } else {
       let resultadoCifrado2 = ""
       for (let i = 0; i < string.length; i++) {
         const decifrar = string.charCodeAt(i);
-        const decodificado = ((decifrar + codigo1Letra - offset) % tamanhoDoAlfabeto) + codigo1Letra ;
+        let decodificado = ((decifrar + codigo1Letra - offset) % tamanhoDoAlfabeto) + codigo1Letra;
+        if (decifrar <= 64 || decifrar >= 91) {
+          decodificado = decifrar;
+        }
         resultadoCifrado2 += String.fromCharCode(decodificado);
       }
       return resultadoCifrado2
-    } 
+    }
   }
 }
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -3,23 +3,23 @@ import cipher from './cipher.js';
 let criptografe = document.getElementById("criptografe")
 
 function botaoCriptar() {
-  let mensagem1 = document.getElementById("msg1").value;
+  let mensagem1 = document.getElementById("msg1").value.toUpperCase();
   let offset = Number(document.getElementById("offset").value);
 
   let textoCifrado = cipher.encode(offset, mensagem1);
-  document.getElementById("resultado1").innerHTML = textoCifrado
+  document.getElementById("msg1").value = textoCifrado;
 }
 criptografe.addEventListener("click", botaoCriptar)
 
 
 
-let descriptar = document.getElementById("descriptar")
+let decriptar = document.getElementById("decriptar")
 
-function botaoDescriptar() {
-  let mensagem2 = document.getElementById("msg2").value;
+function botaoDecriptar() {
+  let mensagem2 = document.getElementById("msg2").value.toUpperCase();
   let offset = Number(document.getElementById("offset").value);
 
   let textoDecifrado = cipher.decode(offset, mensagem2);
-  document.getElementById("resultado2").innerHTML = textoDecifrado
+  document.getElementById("msg2").value = textoDecifrado
 }
-descriptar.addEventListener("click", botaoDescriptar)
+decriptar.addEventListener("click", botaoDecriptar)
